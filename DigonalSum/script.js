a4950802f5d1abeaccc1e572e0a4968ac2a02ed9
gulshan0201 <84889171+gulshan0201@users.noreply.github.com>
2022-05-21 01:09:01 +0530

    Add files via upload

---
+++ DigonalSum/script.js
@@ -0,0 +1,22 @@
+var array = [
+  [1,2,3,4], 
+  [3,4,5,6], 
+  [3,5,7,8], 
+  [7,8,9,1],
+];
+
+var sum = 0;
+function diginalSum(matrix){
+  for(let i=0; i<matrix.length; i++){
+    for(let j=0; j<matrix[i].length; j++){
+      if(i === j){
+        sum = sum + matrix[i][j]
+      }
+    }
+  }
+  return sum;
+}
+var x = diginalSum(array);
+console.log(x);
+
+const ans = document.getElementById('ans').innerHTML = "The answer is " + document.write(x)
